2fed6e9c09a57d724a679c0e6df9e2b763d9751d
tristan-badet <127488323+tristan-badet@users.noreply.github.com>
2023-06-04 13:40:21 +0200

    Tous les boutons fonctionnent

--- crud-application/src/components/Tables.jsx
+++ crud-application/src/components/Tables.jsx
@@ -1,120 +1,151 @@
-import React from 'react'
-import { getPeople, addPerson, deletePerson, editPerson } from "../services/peopleAPI";
-import { useEffect, useState } from 'react'
-
-
+import React, { useEffect, useState } from "react";
+import {
+  getPeople,
+  addPerson,
+  deletePerson,
+  editPerson,
+} from "../services/peopleAPI";
+import styles from "./Tables.module.css";
 
 const Tables = () => {
-    const [formData, setFormData] = useState({
-        id: '',
-        name: '',
-        surname: '',
-        mail: ''
-      });
+  const [formData, setFormData] = useState({
+    id: "",
+    name: "",
+    surname: "",
+    mail: "",
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const handleInputChange = ({ target: { name, value } }) => {
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-      const handleInputChange = (event) => {
-        setFormData({
-          ...formData,
-          [event.target.name]: event.target.value
-        });
-      }
+  const handleEdit = (person) => {
+    setFormData(person);
+    setIsEditing(true);
+  };
 
-      const handleEdit = (person) => {
-        setFormData({
-        id: person.id,
-        name: person.name,
-        surname: person.surname,
-        mail: person.mail
-      })}
-      
-    const [people, setPeople] = useState([]);
-    const handleDelete = async (id) =>{
-      try {
-        await deletePerson(id);
-        setPeople(prevPeople => prevPeople.filter(person => person.id !== id));
-      }catch (error) {
-        console.error(error);
-      }
+  const [people, setPeople] = useState([]);
+  const handleDelete = async (id) => {
+    try {
+      await deletePerson(id);
+      setPeople((prevPeople) =>
+        prevPeople.filter((person) => person.id !== id)
+      );
+    } catch (error) {
+      console.error(error);
     }
+  };
 
   useEffect(() => {
-    const fetchPeople = async () => {
+    (async () => {
       const result = await getPeople();
-      console.log(result);
       setPeople(result);
-    }
-    fetchPeople();
-    console.log(people);
+    })();
   }, []);
-
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    if (formData.id) {
-      try {
+    const idExist = people.some((person) => formData.id === person.id);
+
+    try {
+      let updatedPeople = [...people];
+      let updatedFormData = { id: "", name: "", surname: "", mail: "" };
+
+      if (idExist) {
         const updatedPerson = await editPerson(formData.id, formData);
-        setPeople(prevPeople =>
-          prevPeople.map(person => (person.id === formData.id ? updatedPerson : person))
+        updatedPeople = people.map((person) =>
+          person.id === formData.id ? updatedPerson : person
         );
-        setFormData({ id: '', name: '', surname: '', mail: '' });
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      try {
+        setIsEditing(false);
+      } else {
         const newPerson = await addPerson(formData);
-        setPeople(prevPeople => [...prevPeople, newPerson]);
-        setFormData({ id: '', name: '', surname: '', mail: '' });
-      } catch (error) {
-        console.error(error);
+        updatedPeople = [...people, newPerson];
       }
+
+      setPeople(updatedPeople);
+      setFormData(updatedFormData);
+    } catch (error) {
+      console.error(error);
     }
   };
-
   return (
     <div>
-    <table>
+      <table>
         <thead>
-            <tr>
-                <th>ID</th> 
-                <th>Name</th>
-                <th>Surname</th>
-                <th>Mail</th>
-                <th>Actions</th>
-            </tr>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Mail</th>
+            <th>Actions</th>
+          </tr>
         </thead>
         <tbody>
-            { /* Pour chaque personne dans le fichier JSON on affiche toutes ses informations en ligne dans le tableau */
-            people.map((person)=> (
-                <tr>
-                    <td>{person.id}</td>
-                    <td>{person.name}</td>
-                    <td>{person.surname}</td>
-                    <td>{person.mail}</td>
-                    <td><button onClick={() => handleDelete(person.id)}>Delete</button>
-                    <button onClick={() => handleEdit(person)}>Edit</button></td>
-                </tr>
-            )
-            )
-            }
+          {people.map((person) => (
+            <tr key={person.id}>
+              <td>{person.id}</td>
+              <td>{person.name}</td>
+              <td>{person.surname}</td>
+              <td>{person.mail}</td>
+              <td>
+                <button
+                  className={styles.btn}
+                  onClick={() => handleDelete(person.id)}
+                >
+                  Delete
+                </button>
+                <button
+                  className={styles.btn}
+                  onClick={() => handleEdit(person)}
+                >
+                  Edit
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-    </table>
-    <h3>Add a new person</h3>
-    {/* On créé un formulaire pour pouvoir ajouter une personne au fichier JSON */}
-    <form onSubmit={handleFormSubmit}>
-        <label for="id">ID : </label>
-        <input type="text" name="id" value={formData.id} onChange={handleInputChange} required />
-        <label for="name">Name : </label>
-        <input type="text" name="name" value={formData.name} onChange={handleInputChange} required />
-        <label for="surname">Surname : </label>
-        <input type="text" name="surname" value={formData.surname} onChange={handleInputChange} required />
-        <label for="mail">Mail : </label>
-        <input type="text" name="mail" value={formData.mail} onChange={handleInputChange} required />
-        <button type="submit">Add</button>
+      </table>
+      <h3>Add a new person</h3>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="id">ID : </label>
+        <input
+          type="text"
+          name="id"
+          value={formData.id}
+          onChange={handleInputChange}
+          required
+        />
+        <label htmlFor="name">Name : </label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          required
+        />
+        <label htmlFor="surname">Surname : </label>
+        <input
+          type="text"
+          name="surname"
+          value={formData.surname}
+          onChange={handleInputChange}
+          required
+        />
+        <label htmlFor="mail">Mail : </label>
+        <input
+          type="text"
+          name="mail"
+          value={formData.mail}
+          onChange={handleInputChange}
+          required
+        />
+        <button className={styles.btn} type="submit">
+          {isEditing ? "Edit" : "Add"}
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Tables
+export default Tables;
--- crud-application/src/services/peopleAPI.js
+++ crud-application/src/services/peopleAPI.js
@@ -1,37 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
 
-  export async function getPeople() {
-    try {
-      const response = await axios.get('http://localhost:3000/people');
-      console.log(response);
-      console.log(response.data);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  export async function addPerson(person) {
+export async function getPeople() {
   try {
-    const response = await axios.post('http://localhost:3000/people', person);
+    const response = await axios.get("http://localhost:3000/people");
+    console.log(response);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
   }
 }
-  export async function deletePerson(id) {
-    try {
-      const response = await axios.delete(`http://localhost:3000/people/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
+export async function addPerson(person) {
+  try {
+    const response = await axios.post("http://localhost:3000/people", person);
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
-  export async function editPerson(id, updatedPerson) {
-    try {
-      const response = await axios.put(`http://localhost:3000/people/${id}`, updatedPerson);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
+}
+export async function deletePerson(id) {
+  try {
+    const response = await axios.delete(`http://localhost:3000/people/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
-  
+}
+export async function editPerson(id, updatedPerson) {
+  try {
+    const response = await axios.put(
+      `http://localhost:3000/people/${id}`,
+      updatedPerson
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
